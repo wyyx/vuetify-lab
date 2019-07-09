ee665fd69f6c94867529e0a288c8627a3df09fdf
wyyx <gnehcyx@163.com>
2019-07-10 00:41:42 +0800

    test dynamic-form

--- src/App.vue
+++ src/App.vue
@@ -5,144 +5,85 @@
         <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld />
-      <v-data-table
-        :loading="true"
-        :headers="headers"
-        :items="desserts"
-        class="elevation-1"
-        :pagination.sync="pagination"
-      >
-        <template v-slot:items="props">
-          <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.calories }}</td>
-          <td class="text-xs-right">{{ props.item.fat }}</td>
-          <td class="text-xs-right">{{ props.item.carbs }}</td>
-          <td class="text-xs-right">{{ props.item.protein }}</td>
-          <td class="text-xs-right">{{ props.item.iron }}</td>
-        </template>
-      </v-data-table>
+      <div class="article-collect-template">
+        <div
+          v-for="formControl in articleCollectTemplate.formControls"
+          :key="formControl.name"
+        >
+          <DynamicForm v-bind="formControl"></DynamicForm>
+          <div>{{ formControl }}</div>
+        </div>
+      </div>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
+import DynamicForm from "./components/DynamicForm.component.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  },
+  components: { DynamicForm },
   data() {
     return {
-      pagination: { sortBy: "calories", descending: false },
-      headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "left",
-          sortable: false,
-          value: "name"
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" }
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%"
-        }
-      ]
+      articleCollectTemplate: {
+        type: 123,
+        desc: "文章征集模板",
+        formControls: [
+          {
+            type: "select",
+            name: "category",
+            validation: {
+              required: true
+            },
+            label: "目录",
+            placeholder: "请选择目录",
+            itemList: [
+              {
+                text: "科幻",
+                value: 1
+              },
+              {
+                text: "散文",
+                value: 2
+              },
+              {
+                text: "经济",
+                value: 3
+              }
+            ],
+            model: 0
+          },
+          {
+            type: "text",
+            name: "title",
+            validation: {
+              max: 20
+            },
+            label: "标题",
+            placeholder: "文章标题",
+            hint: "请写一个吸引人的标题",
+            model: ""
+          },
+          {
+            type: "textarea",
+            name: "content",
+            validation: {
+              max: 1000
+            },
+            label: "正文",
+            placeholder: "这里写正文",
+            hint: "正文的字数不能超过500",
+            counter: 1000,
+            maxlength: 1000,
+            model: ""
+          }
+        ]
+      }
     };
   }
 };
---
+++ src/components/DynamicForm.component.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="root">
+    <v-text-field name="name" label="label" id="id"></v-text-field>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: ["name"],
+  created() {
+    const that: any = this;
+    console.log("TCL: created -> this.$props", this.$props);
+    console.log("name", that.name);
+  },
+  mounted() {
+    console.log("TCL: created -> this.$refs.root", this.$refs.root);
+    console.log(
+      "TCL: mounted -> (this.$refs.root as any).validation",
+      this.$refs.root
+    );
+
+    (this.$refs.root as any).getAttribute("validation");
+    console.log(
+      "TCL: mounted -> (this.$refs.root as any).getAttribute('validation')",
+      JSON.stringify((this.$refs.root as any).getAttribute("validation"))
+    );
+  }
+});
+</script>
+
+<style></style>
